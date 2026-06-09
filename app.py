0c2d572ea26caa7f0c84e86e90edb7ebdd45db83
suvasri <suvasri.sukumar@appasamy.com>
2026-06-09 10:56:00 +0530

    Render deployment fix

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def logout():
     return redirect(url_for('login_page'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
